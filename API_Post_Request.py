87bde510237f35834c90964a7b722e1d994685de
arthikapatil28 <arthikapatil28@gmail.com>
2025-08-14 12:58:09 +0530

    Add files via upload

--- API_Post_Request.py
+++ API_Post_Request.py
@@ -1,16 +1,21 @@
 import requests
-from payload import *
-response=requests.post("http://216.10.245.166/Library/Addbook.php",
-                       json={"name":"Python for Beginner-L4",
-                             "isbn":"pybgnL42816",
-                             "aisle":"2021",
-                             "author":"ArthikaPatil"},
+from utilities.payload import *
+from utilities.resources import *
+#config=configparser.ConfigParser()
+#config.read('C:\Users\Patil\PycharmProjects\PythonProject\utilities\properties.ini')
+#config.read('C:/Users/Patil/PycharmProjects/PythonProject/utilities/properties.ini')''''''
+url=getconfig()['API']['apipoint']+'/Library/Addbook.php'
+query='select * from Books'
+response=requests.post(url,
+                       json=buildpayloadfromDB(query),
                        headers={"Content-Type" : "application/json"},)
-print("STATUS OF RESPONSE" ,response.status_code) #200
+print("STATUS OF RESPONSE:\n" ,response.status_code) #200
+information_of_table=getQuery(query)
+print("Information of table been added:\n",information_of_table)
 #print(type(response))
 try:
     data = response.json()
-    print("RESPONSE OF THE API REQUEST",data)  #{'Msg': 'Book Already Exists', 'ID': 'pybgnL228162023'}
+    print("RESPONSE OF THE API REQUEST:\n",data)  #{'Msg': 'Book Already Exists', 'ID': 'pybgnL228162023'}
 except requests.exceptions.JSONDecodeError:
     print("Server did not return JSON. Raw response:")
     print(response.text)
@@ -22,7 +27,7 @@ print("Book ID:", book_id)
 Get_response=requests.get("http://216.10.245.166/Library/GetBook.php",
                           {"AuthorName":"ArthikaPatil"},)
 
-print("LIST OF BOOKS AFTER ADDING BOOK : " ,Get_response.json())
+print("LIST OF BOOKS AFTER ADDING BOOK ::\n " ,Get_response.json())
 '''[{'book_name': 'Learn Appium Automation with Java', 'isbn': 'AP2816', 'aisle': '2025'},
  {'book_name': 'Python', 'isbn': 'pyt2816', 'aisle': '2025'}, 
  {'book_name': 'SQL for Begineer', 'isbn': 'sql2816', 'aisle': '2025'},
@@ -33,13 +38,14 @@ print("LIST OF BOOKS AFTER ADDING BOOK : " ,Get_response.json())
 Delete_response=requests.post("http://216.10.245.166/Library/DeleteBook.php",
                               json=payloaddata(book_id),)
 Ans=Delete_response.json()
-print("DELETE RESPONSE : ",Ans) #{'msg': 'book is successfully deleted'}
+print("Book to be deleted have details as ::\n ", information_of_table )
+print("DELETE RESPONSE ::\n ",Ans) #{'msg': 'book is successfully deleted'}
 assert Ans['msg'] == 'book is successfully deleted'
 #get details of all Books
 
 Get_response=requests.get("http://216.10.245.166/Library/GetBook.php",
                           {"AuthorName":"ArthikaPatil"},)
-print("LIST OF BOOKS AFTER DELETING THE SAME BOOK : " ,Get_response.json())
+print("LIST OF BOOKS AFTER DELETING THE SAME BOOK :\n " ,Get_response.json())
 '''[{'book_name': 'Learn Appium Automation with Java', 'isbn': 'AP2816', 'aisle': '2025'},
  {'book_name': 'Python', 'isbn': 'pyt2816', 'aisle': '2025'}, 
  {'book_name': 'SQL for Begineer', 'isbn': 'sql2816', 'aisle': '2025'},
